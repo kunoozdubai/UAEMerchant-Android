12d1c95d78357eaf333385bcb502f316bc21756b
wackyb <wackythecrab@gmail.com>
2012-10-11 23:22:18 +0500

    added new network constants

---
+++ UAEMerchant/src/com/uaemerchant/common/NetworkConstants.java
@@ -0,0 +1,43 @@
+package com.uaemerchant.common;
+
+
+public class NetworkConstants {
+	
+	
+	
+	public static String UAE_MERCHANT_URL = "http://app.uaemerchant.net/";
+	public static String WS_AD_LIST  = "adlist.php";
+	public static String WS_REGISTER  = "register.php";
+	public static String WS_POST  = "post.php";
+	public static String WS_LOGIN  = "login.php";
+	
+	
+	public static String TITLE = "title";
+	public static String PRICE = "price";
+	public static String USER_ID = "userId";
+	public static String CAT_ID = "catId"; 
+	public static String ADDRESS = "address";
+	public static String LONGITUDE = "longitude";
+	public static String LATITUDE = "latitude";
+	public static String DESCRIPTION = "description";
+	public static String PHOTO_1 = "photo1";
+	public static String PHOTO_2 = "photo2";
+	public static String PHOTO_3 = "photo3";
+	
+	public static String PAGE = "page";
+	public static String LIMIT = "limit"; 
+	public static String KEY = "key";
+	
+	public static String EMAIL = "email";
+	public static String PASSWORD = "password";
+	public static String NAME = "name";
+	public static String PHONE = "phone";
+	public static String CITY = "city";
+	
+	public static String AD_ID = "adId";
+	public static String CREATED = "created";
+	public static String STATUS  = "status";
+	
+	
+			
+}
