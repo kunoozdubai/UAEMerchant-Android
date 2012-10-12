3cfa159cbb4ffb288f0dafdee79bf9eefe57fa89
wackyb <wackythecrab@gmail.com>
2012-10-12 21:24:51 +0500

    new contants

--- UAEMerchant/src/com/uaemerchant/common/CommonConstants.java
+++ UAEMerchant/src/com/uaemerchant/common/CommonConstants.java
@@ -1,12 +1,22 @@
 package com.uaemerchant.common;
 
 
+
 public class CommonConstants {
 	
 	public static final String USER_DEFAULT_PREFERENCES = "UAEMerchatSharedPref";
+	
+	public static final String PREF_NAME = "name"; 
+	public static final String PREF_EMAIL = "email";
+	public static final String PREF_PHONE = "phone";
+	public static final String PREF_CITY = "city";
+	public static final String PREF_USER_ID = "userId"; 
+	
 	
 	public static double LATITUDE;
 	public static double LONGITUDE;
 	
 	public static String MERCHANT_IMAGE_DIR = "/mnt/sdcard/UAEMerchant/";
+	
+
 }
