359a5446a7cdb51ce90b5ecd74af5c7fc13574b2
wackyb <wackythecrab@gmail.com>
2012-10-12 21:21:46 +0500

    implementeatoin of registeration from account screen

--- UAEMerchant/src/com/uaemerchant/dialogs/AccountDialog.java
+++ UAEMerchant/src/com/uaemerchant/dialogs/AccountDialog.java
@@ -1,7 +1,9 @@
 package com.uaemerchant.dialogs;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
@@ -9,8 +11,11 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.uaemerchant.R;
+import com.uaemerchant.common.CommonConstants;
+import com.uaemerchant.common.Utilities;
 
 
 public class AccountDialog extends Dialog implements View.OnClickListener, OnCancelListener{
@@ -24,13 +29,70 @@ public class AccountDialog extends Dialog implements View.OnClickListener, OnCan
 		activity = (Activity) this.context;
 		LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		accountView = layoutInflater.inflate(R.layout.account_dialog, (ViewGroup) activity.findViewById(R.layout.main_activity));
+		setContentView(accountView);
 	}
 	
 		
 	@Override
 	public void show() {
 		super.show();
+		String userId = Utilities.getStringValuesFromPreference(context, CommonConstants.PREF_USER_ID, "");
+		if(!Utilities.isStringEmptyOrNull(userId)){
+			initializeViews();
+		}else{
+			showRegisterAlertDialog();
+		}
 	}
+
+	
+	
+	
+	private void showRegisterAlertDialog() {
+		Builder alertBuilder = new Builder(context);
+        
+		alertBuilder.setTitle("Register!");
+        alertBuilder.setMessage("You need to register to post your ads");
+        
+        alertBuilder.setNegativeButton("No Thanks", new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// Go back to main category activity
+				hide();
+			}
+		});
+
+        alertBuilder.setPositiveButton("Register", new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				/// open registration dialog
+				new RegisterDialog(context).show();
+				hide();
+			}
+		});
+
+        alertBuilder.create().show(); 
+	}
+
+
+	private void initializeViews() {
+		TextView textview = (TextView) accountView.findViewById(R.id.nameTxt);
+		String value = Utilities.getStringValuesFromPreference(context, CommonConstants.PREF_NAME, "");
+		textview.setText(value);
+		
+		textview = (TextView) accountView.findViewById(R.id.emailTxt);
+		value = Utilities.getStringValuesFromPreference(context, CommonConstants.PREF_EMAIL, "");
+		textview.setText(value);
+		
+		textview = (TextView) accountView.findViewById(R.id.phoneTxt);
+		value = Utilities.getStringValuesFromPreference(context, CommonConstants.PREF_PHONE, "");
+		textview.setText(value);
+		
+		textview = (TextView) accountView.findViewById(R.id.cityTxt);
+		value = Utilities.getStringValuesFromPreference(context, CommonConstants.PREF_CITY, "");
+		textview.setText(value);
+
+	}
+
 
 	@Override
 	public void hide() {
@@ -69,6 +131,7 @@ public class AccountDialog extends Dialog implements View.OnClickListener, OnCan
 	public void onCancel(DialogInterface dialog) {
 		context = null;
 		activity = null;
+		Utilities.unbindDrawables(findViewById(R.id.account_dialog));
 	}
 
 
