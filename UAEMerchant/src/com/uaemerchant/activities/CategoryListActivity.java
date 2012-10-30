b202a8095f920eb3897bed1e4ea6e9b6f85a9ebb
wackyb <wackythecrab@gmail.com>
2012-10-30 23:53:40 +0500

    changed imports

--- UAEMerchant/src/com/uaemerchant/activities/CategoryListActivity.java
+++ UAEMerchant/src/com/uaemerchant/activities/CategoryListActivity.java
@@ -17,7 +17,6 @@ import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
